09f124b99428566147a44573f002ac6806891ead
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-12 17:07:54 +0800

    Python module deque
    Implemented the built-in Python module deque to collections for an efficient circular buffer structure.

---
+++ queues.py
@@ -0,0 +1,6 @@
+# Queues Python File
+# In this file we will import the Queues module and create the bare-bones queue object needed for enqueue and dequeue operations.
+
+# Importing the Queues module
+from collections import deque
+
