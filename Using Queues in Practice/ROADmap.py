6242ceb92463917401772c7114bd4ae1c8195367
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-13 01:13:41 +0800

    Imported networkx Module
    With the help of pygraphviz function (third-party library), the object representation of cities and roads from the relative DOT file shall be made possible.

--- Using Queues in Practice/ROADmap.py
+++ Using Queues in Practice/ROADmap.py
@@ -2,3 +2,5 @@
 # Implementing classical graph using queues by visualizing nodes in a sample country that represents a map between roads and cities. Queues contain the general idea for creating an optimal path between cities but merely accounts nothing on the official road types, schedules, rules, capacity, traffic, and other bypasses on that particular information.
 
 # pygraphviz and networkx libraries are the only libraries that would be useful throughout this program as well as the DOT graph file from the source code material.
+import networkx as nx # With the help of pygraphviz (acting as third-party library), it provides functions that delegate this task and manipulate the coordinates of the graph in the sample DOT file.
+print(nx.nx_agraph.read_dot("Stack-Queues-and-Priority-Queues/Using Queues in Practice/roadmap.dot"))
