27cd8b513d880714a9a9ad3d89df60acc13351be
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-12 19:46:52 +0800

    Appended Class Inheritance (See description for details)
    LIFO Commit Changes are of the following:
    
    - Queues() renamed to Stack() to connect the concept goal of the program.
    
    - def dequeue(self) function has changed to reverse the dequeue position index process from first to last.
    
    - Renamed all occurrences to Stack().

--- LIFOqueues.py
+++ LIFOqueues.py
@@ -10,7 +10,7 @@ from collections import deque
 
 # Class variables for the queue implementation of enqueue and dequeue.
 # Improving our enqueued elements on class by making it iterable and be able to report its length and optionally accept initial elements (see the commit for appended code changes).
-class Queue:
+class Stack(): # Class Inheritance
     def __init__(self, * elements):
         self.__elements = deque(elements) # Appending the leading underscore __ on the "element" to provide an internal bit of implementation, which only the class should access and modify.
     
@@ -29,19 +29,19 @@ class Queue:
 
     # Dequeue an element from the queue and return it as a string.
     def dequeue(self):
-        return self.__elements.popleft()
+        return self.__elements.pop() # From popleft() to pop() (to right) specifically for LIFO queues.
 
 # Main functions for testing out our FIFO queue implementation.
-fifo = Queue()
+fifo = Stack() # Added class inheritance for caller functions towards this variable.
 fifo.enqueue('First')
 fifo.enqueue('Second')
 fifo.enqueue('Third')
 
 # Class for testing out our FIFO queue implementation
 def main():
-    fifo = Queue('First', 'Second', 'Third') # Create a FIFO queue with two elements each of which should be enqueued together.
+    fifo = Stack('First', 'Second', 'Third') # Create a FIFO queue with two elements each of which should be enqueued together.
     print(f"\n{len(fifo)} - before iteration (elements are not yet dequeued)") # Len queue is still not called yet so the value here would always be 3.
-    # For loop reduces its size on each single file element to be called once per queue by dequeueing elements from itself as we iterate through them.n
+    # For loop reduces its size on each single file element to be called once per queue by dequeueing elements from itself as we iterate through them.
     for element in fifo: 
         print(element)
     print(f"{len(fifo)} - after iteration (elements are dequeued)\n") # Len queue has been iterated so no element is currently queued, the value here would always be 0.
