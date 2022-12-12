67bb55757b2f5d9c865380022c354b6ad1498799
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-12 17:50:23 +0800

    Improving Class for the following Features:
    - By making it iterate over.
    - Able to report its length.
    - Optionally accept initial elements such as (1st) from the enqueue function.

--- queues.py
+++ queues.py
@@ -7,17 +7,25 @@ from collections import deque
 # from queues import Queue
 
 # Class variables for the queue implementation of enqueue and dequeue.
+# Improving our enqueued elements on class by making it iterable and be able to report its length and optionally accept initial elements (see the commit for appended code changes).
 class Queue:
-    def __init__(self):
-        self.elements = deque()
+    def __init__(self, * elements):
+        self.__elements = deque(elements) # Appending the leading underscore __ on the "element" to provide an internal bit of implementation, which only the class should access and modify.
     
+    def __len__(self):
+        return len(self.__elements)
+    
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+            
     # Enqueue a single element to the queue and return the element itself to the caller as a string.
-    def enqueue(self, value):
-        self.elements.append(value)
+    def enqueue(self, element):
+        self.__elements.append(element)
 
     # Dequeue an element from the queue and return it as a string.
     def dequeue(self):
-        return self.elements.popleft()
+        return self.__elements.popleft()
 
 # Main functions for testing out our FIFO queue implementation.
 fifo = Queue()
