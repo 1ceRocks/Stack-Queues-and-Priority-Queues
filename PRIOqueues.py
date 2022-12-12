968fa61ca8fd90ab49a6de1345f9e541fc3d2ccf
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-12 21:49:25 +0800

    Main Description of the Program
    Listed as a comment on the first few lines of the code.

---
+++ PRIOqueues.py
@@ -0,0 +1,2 @@
+# Priority Queues Python File
+# In this file, we need to create a different hierarchy for each process as the concept program subsides with priority queues. There is also an insertion order for us to determine on which new element is labeled for dequeueing. It's like removing the element from the highest priority as first iteration up to the least priority itself.
