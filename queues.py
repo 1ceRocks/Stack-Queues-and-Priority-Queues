9aab7ec9fb9b1d7d366ada55ea99c90eed699d65
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-12 17:26:03 +0800

    Added Comments for Usage

--- queues.py
+++ queues.py
@@ -1,15 +1,18 @@
 # Queues Python File
 # In this file we will import the Queues module and create the bare-bones queue object needed for enqueue and dequeue operations.
 
-# Importing the Queues module
+# Importing the Queues module from Python
 from collections import deque
 
+# Class variables for the queue implementation of enqueue and dequeue.
 class Queue:
     def __init__(self):
         self.elements = deque()
     
+    # Enqueue a single element to the queue and return the element itself to the caller as a string.
     def enqueue(self, value):
         self.elements.append(value)
 
+    # Dequeue an element from the queue and return it as a string.
     def dequeue(self):
         return self.elements.popleft()  
