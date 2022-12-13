d17f42cc35a1cb8af78a3ac9db5df292762cd146
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-13 20:07:02 +0800

    Map Specifications are Considered
    - node[] argument string "bangor" changed to "bristol"
    
    - Neighboring cities with numerical weights are listed in sorted order from which it is defined from the DOT file.

--- Using Queues in Practice/representGraph.py
+++ Using Queues in Practice/representGraph.py
@@ -3,10 +3,15 @@ from graph import City, load_graph
 
 # Parameters in the load_graph are inherited from the graph.py file.
 nodes, graph = load_graph("Stack-Queues-and-Priority-Queues/Using Queues in Practice/roadmap.dot", City.from_dict)
-print(f"\n{nodes['bangor']}")
+print(f"\n{nodes['bristol']}")
 print(f"{graph}\n")
 
 # The method neighbors() identifies the neighboring cities immediately with nodes[] as the argument within the given subgraph information on the DOT file. Available routes, estimated travel times, distances, possible weights, and connecting edges to follow are still yet to be considered.
-for neighbor in graph.neighbors(nodes["bangor"]):
+for neighbor in graph.neighbors(nodes["bristol"]):
     print(neighbor.name)
+print("")
+
+# In this program output, the map specifications are portrayed in digits to represent their distance.
+for neighbor, weights in graph[nodes["bristol"]].items():
+    print(weights["distance"], neighbor.name)
 print("")
