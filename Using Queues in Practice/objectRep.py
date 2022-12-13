ba8d9fe898e0045716d8eae93863863ed4b378a1
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-13 18:39:40 +0800

    Grammar Spelling Correction

--- Using Queues in Practice/objectRep.py
+++ Using Queues in Practice/objectRep.py
@@ -1,4 +1,4 @@
-# Road Map of the Unkited Kingdom Python File
+# Object Representation of the United Kingdom Python File
 # Implementing classical graph using queues by visualizing nodes in a sample country that represents a map between roads and cities. Queues contain the general idea for creating an optimal path between cities but merely accounts nothing on the official road types, schedules, rules, capacity, traffic, and other bypasses on that particular information.
 
 # pygraphviz and networkx libraries are the only libraries that would be useful throughout this program as well as the DOT graph file from the source code material.
