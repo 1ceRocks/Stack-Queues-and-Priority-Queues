35a0f07c65b7eacb41535337154b880f0f789e72
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-13 18:57:07 +0800

    Configured Data Class
    A later requirement of networkx that is hashable and comparable side by side. This can also represent and visualize the traversal order of the graph.

--- Using Queues in Practice/graph.py
+++ Using Queues in Practice/graph.py
@@ -2,3 +2,12 @@
 
 # From this program, we'll need a module and a class package, known as typing.
 from typing import NamedTuple
+
+# Configured data class for later cases such as the requirement of networkx.
+# This is also comparable that is hashable out of the box which is essential to visualize the traversal order of the graph.
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
