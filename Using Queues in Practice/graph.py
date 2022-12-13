b67ce1c4c319d3003f10c45d4663ab88b852c1b7
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-13 18:46:17 +0800

    Importing Python Class Module

--- Using Queues in Practice/graph.py
+++ Using Queues in Practice/graph.py
@@ -1,1 +1,4 @@
-# Graph 
+# Defining a custom data type for visualizing the thought of graphs in cities and road maps conveniently.
+
+# From this program, we'll need a module and a class package, known as typing.
+from typing import NamedTuple
