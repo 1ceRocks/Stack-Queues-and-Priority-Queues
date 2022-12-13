2d9438c1a0fb53d9acf7d701ffa28fc7f8848358
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-13 20:02:43 +0800

    Displaying Neighboring Cities
    The data information extracted from the DOT file subgraph country are the only details present on the terminal output. Neighboring cities do not have specified travel and map specifications but can detect its terrestrial nodes with average distance to each other.

--- Using Queues in Practice/representGraph.py
+++ Using Queues in Practice/representGraph.py
@@ -3,4 +3,10 @@ from graph import City, load_graph
 
 # Parameters in the load_graph are inherited from the graph.py file.
 nodes, graph = load_graph("Stack-Queues-and-Priority-Queues/Using Queues in Practice/roadmap.dot", City.from_dict)
-print(f"\n{nodes['bangor']}\n")
+print(f"\n{nodes['bangor']}")
+print(f"{graph}\n")
+
+# The method neighbors() identifies the neighboring cities immediately with nodes[] as the argument within the given subgraph information on the DOT file. Available routes, estimated travel times, distances, possible weights, and connecting edges to follow are still yet to be considered.
+for neighbor in graph.neighbors(nodes["bangor"]):
+    print(neighbor.name)
+print("")
