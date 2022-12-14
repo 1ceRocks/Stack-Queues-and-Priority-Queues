d03b868d91e35512ca34e91eec2f7b15427ceed3
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-14 21:31:35 +0800

    Appended .simulate_work() Def Function
    It picks a random delay in seconds to adjust the worker's speed and their progress through their work.

--- Thread Safe Queues/thread_safe_queues.py
+++ Thread Safe Queues/thread_safe_queues.py
@@ -78,3 +78,12 @@ class Worker(threading.Thread):
         self.working = False
         self.progress = 0
         sleep(randint(1, 3))
+
+    # The .simulate_work() picks a random delay in seconds adjusted to the worker’s speed and progresses through the work.
+    def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay / 100)
+            self.progress += 1
