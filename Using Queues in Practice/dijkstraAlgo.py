521a8c199b3f7b9e062caca7bd835c8439a2c8c5
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-14 19:57:41 +0800

    Appending Dijkstra's Algorithm Tester

---
+++ Using Queues in Practice/dijkstraAlgo.py
@@ -0,0 +1,16 @@
+# Test-driving the Dijkstra’s algorithm interactively and compare it against the networkx implementation:
+import networkx as nx
+from graph import City, load_graph, dijkstra_shortest_path
+
+nodes, graph = load_graph("Stack-Queues-and-Priority-Queues/Using Queues in Practice/roadmap.dot", City.from_dict)
+
+city1 = nodes["london"]
+city2 = nodes["edinburgh"]
+
+def distance(weights):
+    return float(weights["distance"])
+
+print("")
+for city in dijkstra_shortest_path(graph, city1, city2, distance):
+    print(city.name)
+print("")
