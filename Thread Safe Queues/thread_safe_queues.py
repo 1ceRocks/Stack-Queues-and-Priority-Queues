9fe5132a991e9870bfd123b7911b7135efaf9376
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-14 20:48:53 +0800

    Initializing Global Namespace
    Importing modules and libraries
    - argparse
    - queues and its classes

---
+++ Thread Safe Queues/thread_safe_queues.py
@@ -0,0 +1,7 @@
+# The thread safe queues is a simultaneous, non-blocking operation cue of multithreaded program that executes more than one flow of execution. By the help of Python language, it provides a few synchronized queue types that we can safely use on multiple threads to facilitate communication between them. Beyond being a valuable algorithmic tool, queues can help abstract away concurrent access to a shared resource.
+
+# We're going to implement a classic multi-producer, multi-consumer problem using this Python program. It usually visualizes / represents a command-line interface script that lets us decide on the number of producers and consumers, their relative speed rates, and the type of queue:
+
+# Importing the necessary module and queue classes into the global namespace.
+import argparse
+from queue import LifoQueue, PriorityQueue, Queue
