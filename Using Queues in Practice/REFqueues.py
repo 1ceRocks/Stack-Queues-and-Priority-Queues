3e4a28741c901bf80c15ddd6bb861ca2b58cd026
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-14 18:15:34 +0800

    Reorganizing the Reference Queues
    With reference to current DFS algorithms, I reconfigured some line codes and appended its structural elements format to behave each stack properly.

--- Using Queues in Practice/REFqueues.py
+++ Using Queues in Practice/REFqueues.py
@@ -3,31 +3,89 @@
 
 # Importing the Queues module from Python
 from collections import deque
+from heapq import heappush, heappop, heapify
+from itertools import count
+from dataclasses import dataclass
+from itertools import count
+from typing import Any
+
 # Importing a local module for testing out our FIFO queue.
 # from queues import Queue
 
-# Class variables for the queue implementation of enqueue and dequeue.
-# Improving our enqueued elements on class by making it iterable and be able to report its length and optionally accept initial elements (see the commit for appended code changes).
-class Queue:
-    def __init__(self, * elements):
-        self.__elements = deque(elements) # Appending the leading underscore __ on the "element" to provide an internal bit of implementation, which only the class should access and modify.
-    
+# Refactoring the Code Using a Mixin Class
+class IterableMixin:                    
     # Compatible with the len() function/s.
     def __len__(self): # Returns the number of elements in the queue (default is 1).
-        return len(self.__elements)
-    
+        return len(self._elements)
+
     # Class instance methods usable such as for loop.
     def __iter__(self): # Returns an iterator of the elements in the queue that are currently enqueued.
         while len(self) > 0:
             yield self.dequeue() # Enqueue the next element in the queue without waiting for the next call to enqueue to complete.
+
+# Class variables for the queue implementation of enqueue and dequeue.
+# Improving our enqueued elements on class by making it iterable and be able to report its length and optionally accept initial elements (see the commit for appended code changes).
+class Queue(IterableMixin):
+    def __init__(self, * elements):
+        self._elements = deque(elements) # Appending the leading underscore __ on the "element" to provide an internal bit of implementation, which only the class should access and modify.
             
     # Enqueue a single element to the queue and return the element itself to the caller as a string.
     def enqueue(self, element):
-        self.__elements.append(element)
+        self._elements.append(element)
 
     # Dequeue an element from the queue and return it as a string.
     def dequeue(self):
-        return self.__elements.popleft()
+        return self._elements.popleft()
+
+# Building Stack(LIFO) data type
+class Stack(Queue): # <-- Inheritance
+    def dequeue(self):
+        return self._elements.pop() # pop() gets and removes the last element on the data.
+
+# Building a Priority Queue Data Type
+class PriorityQueue(IterableMixin):
+    def __init__(self):
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        element = (-priority, next(self._counter), value) # the _counter will be responsible for handling 
+                                                            # the same priority description making the 
+                                                            # first enqueued be the first by default.  
+        heappush(self._elements, element) # rearranging heappush parameters
+    def dequeue(self):
+        return heappop(self._elements)[-1] # to indicate the last component of the tuple, 
+                                            #regardless of its length, -1 was implemented as the index 
+                                            # of the tuple
+
+# Mutable version of a min-heap 
+@dataclass(order=True) # order flag, which makes the elements comparable(just like tuples)
+class Element:
+    priority: float
+    count: int
+    value: Any
+
+class MutableMinHeap(IterableMixin):
+    def __init__(self):
+        super().__init__()
+        self._elements_by_value = {}
+        self._elements = []
+        self._counter = count()
+
+    def __setitem__(self, unique_value, priority):
+        if unique_value in self._elements_by_value:
+            self._elements_by_value[unique_value].priority = priority
+            heapify(self._elements)
+        else:
+            element = Element(priority, next(self._counter), unique_value)
+            self._elements_by_value[unique_value] = element
+            heappush(self._elements, element)
+
+    def __getitem__(self, unique_value):
+        return self._elements_by_value[unique_value].priority
+
+    def dequeue(self):
+        return heappop(self._elements).value
 
 # Main functions for testing out our FIFO queue implementation.
 # This would be unnecessary if elements line 41 was already defined into a variable (fifo).
