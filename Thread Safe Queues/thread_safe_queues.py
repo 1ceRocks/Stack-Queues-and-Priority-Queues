407c02104477fc60f0735073fe815fd4b8e05626
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-14 21:41:04 +0800

    Adding Class View for Structural Pattern Matching

--- Thread Safe Queues/thread_safe_queues.py
+++ Thread Safe Queues/thread_safe_queues.py
@@ -97,3 +97,50 @@ class Worker(threading.Thread):
             self.progress += 1
 
 # The code below defines a view that renders the current state of your producers, consumers, and the queue ten times a second:
+# Notice the use of structural pattern matching to set the title and products based on the queue type. We'll create an instance of the view and call its .animate() method once the producers and consumers are in place.
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
+
+    def animate(self):
+        with Live(
+            self.render(), screen=True, refresh_per_second=10
+        ) as live:
+            while True:
+                live.update(self.render())
+
+    def render(self):
+
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue)
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue))
+            case _:
+                title = products = ""
+
+        rows = [
+            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
+        ]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+
+    def panel(self, worker, title):
+        if worker is None:
+            return ""
+        padding = " " * int(29 / 100 * worker.progress)
+        align = Align(
+            padding + worker.state, align="left", vertical="middle"
+        )
+        return Panel(align, height=5, title=title)
