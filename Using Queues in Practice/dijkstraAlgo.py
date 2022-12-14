46fbc03e46d50ecb87c279563a5df8dc40968199
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-14 20:23:41 +0800

    Dijkstra's Algorithm Finalization

--- Using Queues in Practice/dijkstraAlgo.py
+++ Using Queues in Practice/dijkstraAlgo.py
@@ -14,3 +14,13 @@ print("")
 for city in dijkstra_shortest_path(graph, city1, city2, distance):
     print(city.name)
 print("")
+
+def weight(node1, node2, weights):
+    return distance(weights)
+
+print("")
+for city in nx.dijkstra_path(graph, city1, city2, weight):
+    print(city.name)
+print("")
+
+# Success! Both functions yield exactly the same shortest path between London and Edinburgh.
