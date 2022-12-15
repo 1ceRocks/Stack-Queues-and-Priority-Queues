1dd246cb00538f4a04e5623ee0170efdac4d6523
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-15 15:37:22 +0800

    Job Class Tester Implemented for Priority Queues

---
+++ Asynchronous_Queues/representAsynch.py
@@ -0,0 +1,10 @@
+# Async Priority Queues Class Inheritance Visualization
+# This Python file will automatically import Job from async_queues.py and check if the validation variable with operator equal to the shortest URL path. It corresponds to the smaller values which takes precedence in a min-heap.
+
+from async_queues import Job
+
+job1 = Job("http://localhost/")
+job2 = Job("http://localhost/:8080/")
+validate = job1 < job2
+
+print(f"\n{validate}\n") # This will print out True in the program terminal output.
