8a813bd10bba1a4627b8b4599299d466ba824c85
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-15 17:32:56 +0800

    Implementing a Class and def for Yielding Tuples

--- Interprocess Communication/multiprocess_queue.py
+++ Interprocess Communication/multiprocess_queue.py
@@ -48,4 +48,30 @@ def chunk_indices(length, num_chunks):
         length -= chunk_size
         num_chunks -= 1
 
-# TODO: Each worker will create its own chunk of letter combinations depending on the range of indices supplied in a dequeued task object in order to reduce the cost of data serialization across our processes. For a specific index, you need to locate a letter combination or an n-tuple or m-set. We may create a new class that contains the combination's formula to make our life simpler:
+# * The collection of alphabet letter combinations with a specific length is represented by this custom data type. You may iterate through instances of the Combinations class using a loop thanks to the two special methods and raise the IndexError exception when all combinations have been used.
+class Combinations:
+    def __init__(self, alphabet, length):
+        self.alphabet = alphabet
+        self.length = length
+
+    def __len__(self):
+        return len(self.alphabet) ** self.length
+
+    def __getitem__(self, index):
+        if index >= len(self):
+            raise IndexError
+        return "".join(
+            self.alphabet[
+                (index // len(self.alphabet) ** i) % len(self.alphabet)
+            ]
+            for i in reversed(range(self.length))
+        )
+
+# * Class in the MD5-reversing method and get rid of the "itertools.product" import declaration.
+def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
+    for length in range(1, max_length + 1):
+        for combination in Combinations(alphabet, length):
+            text_bytes = "".join(combination).encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
