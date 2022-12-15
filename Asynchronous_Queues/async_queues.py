e838f8d5a0f0267d4de3b19008cb9b03ddfe9b4a
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-15 13:25:22 +0800

    JavaScript Parse Link for HTML Hyperlinks
    We also supplemented some Better Comment Line Blocks to better visualize the context of the program as it passes data asynchronously.

--- Asynchronous_Queues/async_queues.py
+++ Asynchronous_Queues/async_queues.py
@@ -2,9 +2,9 @@
 
 # In a synchronous environment, a program execution follows a set of operations sequentially. The execution flow will start processing a step and wait for it to return a result before proceeding to the next one. With asynchronous programming, we can use the lag time required by the operation to process and return a result to continue processing other tasks. This slows down the program as it is forced to stop and wait for something to finish. Many processors are available in the system, so it is a waste of resources to do other tasks rather than the ideal sit.
 
-# Necessary libraries for parallel execution are defined in the following modules that can be used.
-# aiohttp - to fetch data asynchronously
-# beautifulsoup4 - to parse HTML Hyperlinks
+# * Necessary libraries for parallel execution are defined in the following modules that can be used.
+# ? aiohttp - to fetch data asynchronously
+# ? beautifulsoup4 - to parse HTML Hyperlinks
 
 # Layout for the web crawler.
 
@@ -14,7 +14,7 @@ from collections import Counter
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-# Defines a counter on the visited links on the HTML hyperlinks being parsed. It also passes the coroutine execution to asyncio.run() for it to have a default event loop session. The coroutine then would pass back and display the list of links sorted by the number of visits in descending order.
+# * Defines a counter on the visited links on the HTML hyperlinks being parsed. It also passes the coroutine execution to asyncio.run() for it to have a default event loop session. The coroutine then would pass back and display the list of links sorted by the number of visits in descending order.
 async def main(args):
     session = aiohttp.ClientSession()
     try:
@@ -23,7 +23,7 @@ async def main(args):
     finally:
         await session.close()
 
-# The 2 define block codes [parse_args() and display(links)]Coroutine receives few command-line interface and returns them with parsed arguments.
+# * The 2 define block codes [parse_args() and display(links)]Coroutine receives few command-line interface and returns them with parsed arguments.
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
@@ -35,14 +35,22 @@ def display(links):
     for url, count in links.most_common():
         print(f"{count:>3} {url}")
 
-# This counter is used internally by the asyncio module to track the number of concurrent requests that are queued to the same page. It also acts as the global workspace for running through parallel processes.
+# * This counter is used internally by the asyncio module to track the number of concurrent requests that are queued to the same page. It also acts as the global workspace for running through parallel processes.
 if __name__ == "__main__":
     asyncio.run(main(parse_args()))
 
-# This will only inherit and return an HTML content from the .content_type operator tag.
+# * This will only inherit and return an HTML content from the .content_type operator tag.
 async def fetch_html(session, url):
     async with session.get(url) as response:
         if response.ok and response.content_type == 'text/html':
             return await response.text()
 
-# TODO: We'll now implement an extracted links from the HTML document and skip the inline JavaScript with the use of href() attribute, which will incorporate a relative path with the current global machine URL.
+# * Inline JavaScript that joins a relative path with the current global machine URL.
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features="html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
+
+# TODO: 
