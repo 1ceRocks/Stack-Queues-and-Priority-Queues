1faf89bd0f06116ea3e07bf510780ac359ad59ae
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 16:20:31 +0800

    Constructor for Consumer Now Selects Objects
    After naming the variable "consumer" in which we have a class KafkaConsumer() and its argument datascience, we utilized a for loop program for message, inputting every element in decoding the value method utf-8 with validation print() function message.

--- Distributed_Message_Queues/Kafka/consumer-kafka.py
+++ Distributed_Message_Queues/Kafka/consumer-kafka.py
@@ -2,4 +2,11 @@
 
 # * Naturally, we have only begun to scrape the surface of what these potent message brokers are capable of. Our objective in reading this part was to gain a fast overview and a place to start in case we wanted to further investigate them.
 
-# TODO: The constructor of the consumer selects one or more subjects in which it could be interested.
+# * Essentially constructor of Kafka Python 3 Message one or more consumer classes that we are interested in reading from here.
+from kafka3 import KafkaConsumer
+
+# * The constructor of the consumer now selects one or more subjects in which the program could be interested.
+consumer = KafkaConsumer("datascience")
+for record in consumer:
+    message = record.value.decode("utf-8")
+    print(f"Got message: {message}")
