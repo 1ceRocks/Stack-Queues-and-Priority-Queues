6097a6598a5dd6a6265529c4ab8f3da6c4f58b78
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 15:23:30 +0800

    TODO List and Importing Redis Module for SQL
    At this rate, we will examine the official demonstration's list commands and executing them when we are already connected to Redis server.

---
+++ Distributed_Message_Queues/Redis/publisher.py
@@ -0,0 +1,6 @@
+# The name Redis, which stands for Remote Dictionary Server, really refers to a variety of things. It's an in-memory key-value data store that often serves as an incredibly quick cache between a server and a standard SQL database. It can function both as a publish-subscribe message broker and a persistent NoSQL database at the same time. With Docker, we may launch a local Redis server:
+
+# Importing redis module to access memory key-value data cache between a traditional SQL database and a server.
+import redis
+
+# TODO: Examine the official documentation's list of commands and give them a try when we're connected to the Redis server. We might just just start using Python. Redis is the first library listed on the official Redis page, but it's important to note that there are a number of other options available, including asynchronous ones. It only takes a few lines of Python code to create a simple publisher.
