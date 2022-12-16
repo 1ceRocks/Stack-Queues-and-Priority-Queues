7aa7484d6771df2a6301b7d728c6a44920d903db
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 14:43:46 +0800

    TODO List for Consumer Python File Program

---
+++ Distributed_Message_Queues/RabbitMQ/consumer.py
@@ -0,0 +1,4 @@
+# Consumer Python File
+# The majority of the boilerplate code resembles your producer. However, since the consumer will continue to listen for messages forever, you do not need to create an explicit loop.
+
+# TODO: Launch a few producers and consumers in different terminal tabs. Take note of what transpires if more than one consumer is connected to the broker or if the queue already contains some unconsumed messages when the first consumer connects to RabbitMQ.
