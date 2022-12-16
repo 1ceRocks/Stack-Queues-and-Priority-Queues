9302244d0f949be7779d7f9e535c41331e728fa7
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 16:08:43 +0800

    Importing Kafka Module
    Kafka-Python-3 supports the version for Python client library because it is based on Java clients,

---
+++ Distributed_Message_Queues/Kafka/producer-kafka.py
@@ -0,0 +1,4 @@
+# Producer Kafka Python 3 File
+
+# ? When Kafka and our client library have different versions, we could occasionally encounter problems. Kafka-python3, which is based on the Java client, is a Python library that appears to work with a more current version of Kafka. Our producer has the following options for communications on a certain subject:
+from kafka3 import KafkaProducer
