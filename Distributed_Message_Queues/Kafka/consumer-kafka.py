4e0902220f14487d1a4d8c96b9cceaf19033f28e
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 16:18:00 +0800

    Comment Block Codes for Consumer Kafka Program

---
+++ Distributed_Message_Queues/Kafka/consumer-kafka.py
@@ -0,0 +1,5 @@
+# Consumer Kafka Python 3 File
+
+# * Naturally, we have only begun to scrape the surface of what these potent message brokers are capable of. Our objective in reading this part was to gain a fast overview and a place to start in case we wanted to further investigate them.
+
+# TODO: The constructor of the consumer selects one or more subjects in which it could be interested.
