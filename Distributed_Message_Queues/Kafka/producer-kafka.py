2b11334616689afb89c080110c37e80de4020b8f
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 16:14:05 +0800

    Added Line Arguments for KafkaProducer Bootstrap
    The .send() method implies asynchronous function. The program provides a future object that we await by invoking its blocking.get() function. On the consumer side, we'll be able to read the transmitted messages with the topic data science that encodes utf-8 by iterating it over the consumer using localhost:9092 bootstrap servers.

--- Distributed_Message_Queues/Kafka/producer-kafka.py
+++ Distributed_Message_Queues/Kafka/producer-kafka.py
@@ -2,3 +2,12 @@
 
 # ? When Kafka and our client library have different versions, we could occasionally encounter problems. Kafka-python3, which is based on the Java client, is a Python library that appears to work with a more current version of Kafka. Our producer has the following options for communications on a certain subject:
 from kafka3 import KafkaProducer
+
+# * Because it provides a future object that we may await by invoking its blocking.get() function, the.send() method is asynchronous. On the consumer’s side, we'll be able to read the transmitted messages by iterating over the consumer:
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+while True:
+    message = input("Message: ")
+    producer.send(
+        topic="datascience",
+        value=message.encode("utf-8"),
+    )
