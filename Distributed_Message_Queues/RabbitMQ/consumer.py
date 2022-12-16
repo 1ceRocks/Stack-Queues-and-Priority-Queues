f0fb303595a9aadd261e32a614f8a48dce35f386
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2022-12-16 14:47:18 +0800

    Implemented Program Sequence Blocks
    def callback() with its four parameters typically involves a variable named "message" that acts as a method decoder in a common computer language and  a print function indicating the received message.
    
    With pika.Blockconnection(), we create a series of queue connection channels so that the QUEUE_NAME with string mailbox can callback on message and starts consuming it with a method shown at line 23.

--- Distributed_Message_Queues/RabbitMQ/consumer.py
+++ Distributed_Message_Queues/RabbitMQ/consumer.py
@@ -2,3 +2,22 @@
 # The majority of the boilerplate code resembles your producer. However, since the consumer will continue to listen for messages forever, you do not need to create an explicit loop.
 
 # TODO: Launch a few producers and consumers in different terminal tabs. Take note of what transpires if more than one consumer is connected to the broker or if the queue already contains some unconsumed messages when the first consumer connects to RabbitMQ.
+
+# * Importing pika to supplement RabbitMQ message broker.
+import pika
+
+QUEUE_NAME = "mailbox"
+
+def callback(channel, method, properties, body):
+    message = body.decode("utf-8")
+    print(f"Got message: {message}")
+
+with pika.BlockingConnection() as connection:
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME)
+    channel.basic_consume(
+        queue=QUEUE_NAME,
+        auto_ack=True,
+        on_message_callback=callback
+    )
+    channel.start_consuming()
